8657601271e60c756c61776bcd87c375d2313ca9
Sy <elitonjr1@gmail.com>
2024-04-09 11:41:31 -0300

    cleanup

---
+++ pogo_trainer_status/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <div>
+    <p>eliton</p>
+   </div>
+  </React.StrictMode>
+);
+
